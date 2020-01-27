5dee44a89fdba89736001e5bdbbf35faa04b1b57
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-01-27 18:00:09 -0500

    up

--- Assignment0/src/Assignment0/Shape.java
+++ Assignment0/src/Assignment0/Shape.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Assignment0;
 
-/**
- *
- * @author tuh12085
- */
+
 public class Shape{
     
     private String name;
--- Assignment0/src/Assignment0/Square.java
+++ Assignment0/src/Assignment0/Square.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Assignment0;
 
-/**
- *
- * @author tuh12085
- */
 public class Square extends Shape {
     public Square(String name) {
         super(name);

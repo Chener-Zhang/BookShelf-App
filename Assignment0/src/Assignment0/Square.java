660242b7e1ac389a37fde7cb4cb0f96f2cd722ea
Chener Zhang <tuh12085@temple.edu>
2020-01-27 15:12:52 -0500

    add the file of Assignment0

---
+++ Assignment0/src/Assignment0/Square.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment0;
+
+/**
+ *
+ * @author tuh12085
+ */
+public class Square extends Shape {
+    public void Square(int length, int height){
+        
+    }
+    
+}
---
+++ Assignment0/src/Assignment0/Triangle.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment0;
+
+/**
+ *
+ * @author tuh12085
+ */
+public class Triangle extends Shape{
+    
+    // a,b,c indicates different sides of the triangle
+    public void Triangle(int a, int b, int c){
+        
+    }
+    
+}

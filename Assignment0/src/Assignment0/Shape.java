ae19dc3177c69d39a798dfc7d77869f2f92c57d0
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-01-27 22:20:32 -0500

    update for change

--- Assignment0/src/Assignment0/Shape.java
+++ Assignment0/src/Assignment0/Shape.java
@@ -6,25 +6,27 @@ public class Shape{
     
     private String name;
     
-    
+
+    // Init for the shape
     public Shape(String name)
     {
-
         this.name = name;
     }
-    
-    
+
+    //get the name of the input
     public String getName() {
-        System.out.println(name);
+        //System.out.println(name);
         return name;
     }
     
-    
+    //Return the Area of the shape
     public double getArea() {
         return 0;
     }
-    
+
+    //print the dimensions of the shape
     public void printDimensions()
+
     {
         System.out.println("No dimensions");
     }

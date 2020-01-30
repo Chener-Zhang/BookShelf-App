82edb79a40fc008a7e3ba49a3933c2bb6bb84afd
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-01-29 22:32:56 -0500

    1: add the function for click on the buttom

--- Assignment1/app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ Assignment1/app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -9,24 +9,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
-    Button bt001 =  (Button)findViewById(R.id.button001);
-    TextView textView001 = (TextView)findViewById(R.id.textView001);
-
-
+    Button mybutton001;
+    TextView mytest001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bt001.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                textView001.setText("You Clicked On Button");
-            }
 
+         mybutton001 = (Button) findViewById(R.id.button001);
+         mytest001 =(TextView) findViewById(R.id.textView001);
+
+        mybutton001.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mytest001.setText("you have press the button!!!");
+            }
         });
     }
-
 
 
 }

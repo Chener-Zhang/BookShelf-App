50627fed4a80b42dc254ccff4b97309615c08ee9
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-02-29 14:07:08 -0500

    updatge

---
+++ Assignment5/app/src/main/java/edu/temple/assignment5/CanvasActivitiy.java
@@ -0,0 +1,43 @@
+package edu.temple.assignment5;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class CanvasActivitiy extends AppCompatActivity {
+
+
+    String getcolor;
+    int getcolor_index;
+    String[] list;
+
+    TextView box;
+
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canvas);
+        box = (TextView) findViewById(R.id.color_text_view);
+
+        getcolor = getIntent().getExtras().getString("color");
+        getcolor_index = getIntent().getExtras().getInt("color_index");
+        list = getIntent().getExtras().getStringArray("list");
+        box.setText(getcolor);
+
+        System.out.println("-----------");
+        System.out.println("index: "+ getcolor_index);
+        System.out.println(list[getcolor_index]);
+        System.out.println(getcolor);
+        System.out.println("-----------");
+
+
+        ConstraintLayout canvas = (ConstraintLayout) findViewById(R.id.canvas_layout);
+        canvas.setBackgroundColor(Color.parseColor(list[getcolor_index]));
+    }
+
+}

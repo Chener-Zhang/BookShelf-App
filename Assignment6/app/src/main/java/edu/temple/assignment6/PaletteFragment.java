5d25ce60938e5842a5651c3c303493208ef8cd02
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-03-15 18:09:31 -0400

    add

--- Assignment6/app/src/main/java/edu/temple/assignment6/PaletteFragment.java
+++ Assignment6/app/src/main/java/edu/temple/assignment6/PaletteFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.provider.FontRequest;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +12,23 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class PaletteFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
 
-    View myview;
-    Spinner My_Spinner;
-    public PaletteFragment(){
+    View MYVIEW;
+    Spinner MY_SPINNER;
+
+    String[] MYCOLORLIST;
+    String[] MYCOLORLIST_HEX;
+
+    Map MY_COLOR_MAP;
+    int LENGTH_OF_COLOR;
+
+   public PaletteFragment() {
 
     }
 
@@ -25,18 +36,39 @@ public class PaletteFragment extends Fragment implements AdapterView.OnItemSelec
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_palette, container, false);
-        myview = view;
+        MYVIEW = view;
+
+        //map set-up
+        map_setup();
+        //spinner set-up
         setup_spinner_adaptor();
-        My_Spinner.setOnItemSelectedListener(this);
+        MY_SPINNER.setOnItemSelectedListener(this);
+        //spinner set-up finished
+
 
         return view;
     }
 
+    public void map_setup(){
+        //Get the list
+        MYCOLORLIST = getResources().getStringArray(R.array.color_list);
+        MYCOLORLIST_HEX = getResources().getStringArray(R.array.color_hex);
+
+        Map<String, String> Color_map = new HashMap<String, String>();
+
+        int len = MYCOLORLIST.length;
+        LENGTH_OF_COLOR = len;
+        for (int i = 0; i < len; i++) {
+        Color_map.put(MYCOLORLIST[i], MYCOLORLIST_HEX[i]);
+        }
+        MY_COLOR_MAP = Color_map;
+    }
 
 
     public void setup_spinner_adaptor() {
         //get the spinner from the PaletteFtagment
-        Spinner myspinner = (Spinner) myview.findViewById(R.id.palette_spinner);
+        Spinner myspinner = (Spinner) MYVIEW.findViewById(R.id.palette_spinner);
+
         //Create a color_list import from Resourse color
         String[] color_list = getResources().getStringArray(R.array.color_list);
 
@@ -45,15 +77,19 @@ public class PaletteFragment extends Fragment implements AdapterView.OnItemSelec
 
         //Connect the spinner with my adapter
         myspinner.setAdapter(ColorAdapter);
-        My_Spinner = myspinner;
-
+        MY_SPINNER = myspinner;
     }
 
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String choosen = parent.getSelectedItem().toString();
-        System.out.println(choosen);
+        if(MY_COLOR_MAP.get(choosen)!=null){
+            System.out.println(choosen);
+            System.out.println("you have this color");
+            
+        }
+
     }
 
     @Override

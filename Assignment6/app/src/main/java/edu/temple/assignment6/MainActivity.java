375fa95fbd7b49dd4892dc9f8e95ce50fb070e70
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-03-15 17:05:40 -0400

    + add 2 arrays string

--- Assignment6/app/src/main/java/edu/temple/assignment6/MainActivity.java
+++ Assignment6/app/src/main/java/edu/temple/assignment6/MainActivity.java
@@ -3,6 +3,8 @@ package edu.temple.assignment6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //------------------------>
+        setup_spinner_adaptor();
     }
+
+    public void setup_spinner_adaptor(){
+
+        //get the spinner from the PaletteFtagment
+        Spinner myspinner = (Spinner) findViewById(R.id.palette_spinner);
+
+        //Create a color_list import from Resourse color
+        String[] color_list =  getResources().getStringArray(R.array.color_list);
+
+        //Making an adapter
+        ArrayAdapter<String> ColorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, color_list);
+
+        //Connect the spinner with my adapter
+        myspinner.setAdapter(ColorAdapter);
+    }
+
+
 }
--- Assignment6/app/src/main/java/edu/temple/assignment6/PaletteFragment.java
+++ Assignment6/app/src/main/java/edu/temple/assignment6/PaletteFragment.java
@@ -7,22 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 public class PaletteFragment extends Fragment {
 
-
-    public PaletteFragment() {
-        // Required empty public constructor
-    }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_palette, container, false);
     }
+
+
+
 
 }

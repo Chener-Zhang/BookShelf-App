404d71d0f1404510d952b60521dcdfcebb83c46a
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-03-25 00:02:09 -0400

    + add the new item

---
+++ Assignment7/app/src/main/java/edu/temple/assignment7/BookListFragment.java
@@ -0,0 +1,27 @@
+package edu.temple.assignment7;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BookListFragment extends Fragment {
+
+    public BookListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_book_list, container, false);
+    }
+}

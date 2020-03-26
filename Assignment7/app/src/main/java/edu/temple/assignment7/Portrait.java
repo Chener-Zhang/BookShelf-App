0540857e35cb7f1165fd605a7a96ae4b6119fcbf
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-03-26 15:45:47 -0400

    update

--- Assignment7/app/src/main/java/edu/temple/assignment7/Portrait.java
+++ Assignment7/app/src/main/java/edu/temple/assignment7/Portrait.java
@@ -3,10 +3,12 @@ package edu.temple.assignment7;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 
@@ -40,6 +42,19 @@ public class Portrait extends Fragment {
         ListView mylist = view.findViewById(R.id.fragment_list_for_portrait);
         mylist.setAdapter(adaptor);
 
+
+
+        mylist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            FragmentManager fragmentManager = getFragmentManager();
+            book_detail_fragment book_detail_fragment = new book_detail_fragment();
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String book = library.getSingleBook(position);
+                String author = library.getSingleBookAuthor(book);
+                fragmentManager.beginTransaction().replace(,book_detail_fragment).commit();
+
+            }
+        });
         
     }
 

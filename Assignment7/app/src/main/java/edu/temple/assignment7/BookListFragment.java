5fa60b3185fb7e9314c8bf7dba9cc1ff0f5c2ac1
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-03-25 22:15:52 -0400

    + add the main_activity.xml + new feature for checking the rotation + turn on the auto-rotation + fix the issue with the string comapre + create the getinstance method + pass the bundle inside the fragment

--- Assignment7/app/src/main/java/edu/temple/assignment7/BookListFragment.java
+++ Assignment7/app/src/main/java/edu/temple/assignment7/BookListFragment.java
@@ -4,18 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 
 public class BookListFragment extends Fragment {
 
+    private static String view_type = "view_type";
     MyLibrary my_library;
     View view;
 
@@ -24,21 +20,44 @@ public class BookListFragment extends Fragment {
     }
 
 
+    public static BookListFragment newInstance(String viewtype) {
+        BookListFragment fragment = new BookListFragment();
+        Bundle args = new Bundle();
+        args.putString(view_type, viewtype);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.portrait, container, false);
+        View view;
         this.my_library = new MyLibrary(getActivity());
         this.my_library.init();
-        this.view = view;
+        Bundle bundle = getArguments();
 
-        list_view_adaptor();
-        return view;
+        if (bundle != null) {
+            String type = bundle.getString(view_type);
+            System.out.println("you type of view is : " + type);
+
+            if (type.equals("p")) {
+                view = inflater.inflate(R.layout.portrait, container, false);
+                this.view = view;
+                list_view_adaptor_portrait();
+            } else if (type.equals("l")) {
+                view = inflater.inflate(R.layout.landscape, container, false);
+                this.view = view;
+            }
+
+        }
+
+
+        return this.view;
     }
 
-    public void list_view_adaptor() {
+    public void list_view_adaptor_portrait() {
         two_item_adaptor adaptor = new two_item_adaptor(getContext(), my_library);
-        ListView book_list = (ListView) view.findViewById(R.id.portrait_xml_ListView);
+        ListView book_list = view.findViewById(R.id.portrait_xml_ListView);
         book_list.setAdapter(adaptor);
 
     }
--- Assignment7/app/src/main/java/edu/temple/assignment7/MainActivity.java
+++ Assignment7/app/src/main/java/edu/temple/assignment7/MainActivity.java
@@ -1,7 +1,7 @@
 package edu.temple.assignment7;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
@@ -12,18 +12,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.portrait);
-
-
+        setContentView(R.layout.main_activitiy);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        BookListFragment bookListFragment = new BookListFragment();
+        BookListFragment bookListFragment = null;
+
+        //TextView test = findViewById(R.id.testing_rotation);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            bookListFragment = BookListFragment.newInstance("p");
+            //test.setText("ORIENTATION_PORTRAIT");
+        } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            bookListFragment = BookListFragment.newInstance("l");
+            //test.setText("ORIENTATION_LANDSCAPE");
+
+        }
+
         fragmentTransaction.replace(android.R.id.content, bookListFragment);
         fragmentTransaction.commit();
-
-
-
 
     }
 

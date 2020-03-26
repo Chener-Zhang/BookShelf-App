d56c4b17a6bdda49261c46d0fffcbfb8af2811c9
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-03-26 16:50:33 -0400

    add the landscape information

--- Assignment7/app/src/main/java/edu/temple/assignment7/Landscape.java
+++ Assignment7/app/src/main/java/edu/temple/assignment7/Landscape.java
@@ -3,16 +3,20 @@ package edu.temple.assignment7;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Landscape extends Fragment {
+
+
+    View view;
+    MyLibrary library;
 
     public Landscape() {
         // Required empty public constructor
@@ -22,6 +26,37 @@ public class Landscape extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_landscape, container, false);
+
+
+        view = inflater.inflate(R.layout.fragment_landscape, container, false);
+        library = new MyLibrary(getContext());
+        library.init();
+        landscape_adaptor();
+        return view;
+
     }
+
+
+
+    public void landscape_adaptor(){
+
+        Book_Adaptor adaptor = new Book_Adaptor(getContext(),library);
+        ListView mylist = view.findViewById(R.id.testlistview);
+        mylist.setAdapter(adaptor);
+
+
+        mylist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            FragmentManager fragmentManager = getFragmentManager();
+            book_detail_fragment book_detail_fragment = new book_detail_fragment();
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String book = library.getSingleBook(position);
+                String author = library.getSingleBookAuthor(book);
+                System.out.println("author " + author + "book " + book);
+                fragmentManager.beginTransaction().replace(R.id.book_detail_fragment_container,book_detail_fragment).commit();
+            }
+        });
+
+    }
+
 }
--- Assignment7/app/src/main/java/edu/temple/assignment7/book_detail_fragment.java
+++ Assignment7/app/src/main/java/edu/temple/assignment7/book_detail_fragment.java
@@ -11,10 +11,23 @@ import android.view.ViewGroup;
 
 public class book_detail_fragment extends Fragment {
 
+
+
+    private static String name = "book_name";
+    private static String author = " book_author";
     public book_detail_fragment() {
-        // Required empty public constructor
+
     }
 
+    public static book_detail_fragment newInstance(String bookname, String bookauthor) {
+        book_detail_fragment myFragment = new book_detail_fragment();
+        Bundle args = new Bundle();
+        args.putString(name, bookname);
+        args.putString(author, bookauthor);
+
+        myFragment.setArguments(args);
+        return myFragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,

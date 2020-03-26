39a6f6bb323f9a0c7b30157b0309012a6f1b389f
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-03-26 14:19:14 -0400

    Restart

--- Assignment7/app/src/main/java/edu/temple/assignment7/MainActivity.java
+++ Assignment7/app/src/main/java/edu/temple/assignment7/MainActivity.java
@@ -14,21 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activitiy);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        BookListFragment bookListFragment = null;
 
-
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            bookListFragment = BookListFragment.newInstance("p");
-
-        } else if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            bookListFragment = BookListFragment.newInstance("l");
-
-        }
-
-        fragmentTransaction.replace(android.R.id.content, bookListFragment);
-        fragmentTransaction.commit();
 
     }
 

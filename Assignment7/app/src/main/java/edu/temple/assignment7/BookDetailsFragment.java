cd6e2c36d750d9af018c49246642891d845cf3ee
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-03-26 00:04:24 -0400

    Revert "Revert "before crash for the fragemtn in the right""
    This reverts commit b0f84a65d72d7a47e14cbf5795d9d2c33b4050d5.

--- Assignment7/app/src/main/java/edu/temple/assignment7/BookDetailsFragment.java
+++ Assignment7/app/src/main/java/edu/temple/assignment7/BookDetailsFragment.java
@@ -4,24 +4,48 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class BookDetailsFragment extends Fragment {
+
+    public static String BOOK = "book";
+    public static String AUTHUR = "author";
 
     public BookDetailsFragment() {
         // Required empty public constructor
     }
 
+    public static BookDetailsFragment newInstance(String book, String author) {
+        BookDetailsFragment fragment = new BookDetailsFragment();
+        Bundle args = new Bundle();
+        args.putString(BOOK, book);
+        args.putString(AUTHUR, author);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_details, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_book_details, container, false);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+
+            String book_name = bundle.getString(BOOK);
+            String author_name = bundle.getString(AUTHUR);
+
+            TextView book_name_textview = view.findViewById(R.id.book_canvas);
+            TextView author_name_textview = view.findViewById(R.id.author_canvas);
+
+            book_name_textview.setText(book_name);
+            author_name_textview.setText(author_name);
+        }
+
+        return view;
     }
+
 }

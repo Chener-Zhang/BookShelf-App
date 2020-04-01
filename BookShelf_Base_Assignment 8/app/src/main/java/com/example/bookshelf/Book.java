dcd8b6566b9a7f663802296d5aee393233c063a7
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-04-01 10:31:53 -0400

    create the instance

--- BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/Book.java
+++ BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/Book.java
@@ -2,6 +2,34 @@ package com.example.bookshelf;
 
 public class Book {
 
+    private int ID;
+    private String TITLE;
+    private String AUTHOR;
+    private String COVER_URL;
+
+    public Book(int id, String title, String author, String cover_url) {
+        ID = id;
+        TITLE = title;
+        AUTHOR = author;
+        COVER_URL = cover_url;
+    }
+
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getTITLE() {
+        return TITLE;
+    }
+
+    public String getAUTHOR() {
+        return AUTHOR;
+    }
+
+    public String getCOVER_URL() {
+        return COVER_URL;
+    }
 }
 
 

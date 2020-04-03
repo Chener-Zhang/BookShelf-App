e645fb20acbdd265ad9b60369930e67b2506d887
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-04-03 16:59:36 -0400

    add img

--- BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/BookDetailsFragment.java
+++ BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/BookDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.example.bookshelf;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,8 +9,15 @@ import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 public class BookDetailsFragment extends Fragment {
 
@@ -17,6 +25,7 @@ public class BookDetailsFragment extends Fragment {
     private Book book;
     private static final String SAVE_BOOK = "save_book";
     TextView titleTextView, authorTextView;
+    ImageView imageView;
 
     public BookDetailsFragment() {
     }
@@ -63,6 +72,8 @@ public class BookDetailsFragment extends Fragment {
 
         titleTextView = v.findViewById(R.id.titleTextView);
         authorTextView = v.findViewById(R.id.authorTextView);
+        imageView = v.findViewById(R.id.my_image);
+
 
         if (book != null) {
             displayBook(book);
@@ -76,5 +87,37 @@ public class BookDetailsFragment extends Fragment {
     public void displayBook(Book book) {
         titleTextView.setText(book.getTITLE());
         authorTextView.setText(book.getAUTHOR());
+        GET_IMG img = new GET_IMG(imageView);
+        img.execute(book.getCOVER_URL());
+    }
+
+
+    private class GET_IMG extends AsyncTask<String,Void,Bitmap>{
+        ImageView imageView;
+        public GET_IMG(ImageView img){
+            this.imageView = img;
+        }
+
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String link = strings[0];
+            Bitmap map = null;
+            try{
+                InputStream inputStream = new java.net.URL(link).openStream();
+                map = BitmapFactory.decodeStream(inputStream);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return map;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }

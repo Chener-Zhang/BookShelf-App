13ae6173908ec98098d7db4651aec94539a60fed
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-04-03 11:46:46 -0400

    finished !!!
    + filter success

--- BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/BookListFragment.java
+++ BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/BookListFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Filter;
@@ -28,7 +29,6 @@ public class BookListFragment extends Fragment {
     private ArrayList<Book> books;
     BookSelectedInterface parentActivity;
     Context main_context;
-
 
 
     public BookListFragment() {
@@ -68,28 +68,18 @@ public class BookListFragment extends Fragment {
         ListView listView = (ListView) inflater.inflate(R.layout.fragment_book_list, container, false);
 
 
-        final BooksAdapter adapter = new BooksAdapter(getContext(),books);
+        final BooksAdapter adapter = new BooksAdapter(getContext(), books);
         listView.setAdapter(adapter);
 
-        EditText search_bar = getActivity().findViewById(R.id.my_search_bar);
+        final EditText search_bar = getActivity().findViewById(R.id.my_search_bar);
+        Button search_button = getActivity().findViewById(R.id.search_button);
 
-
-
-        search_bar.addTextChangedListener(new TextWatcher() {
-
+        search_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    adapter.getFilter().filter(s);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
+            public void onClick(View v) {
+                String user_input = search_bar.getText().toString();
+                System.out.println("you have enter : " + user_input);
+                adapter.getFilter().filter(user_input);
             }
         });
 

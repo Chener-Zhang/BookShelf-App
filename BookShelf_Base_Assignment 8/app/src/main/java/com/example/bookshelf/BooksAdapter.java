565d949dd1af36a8ff015bb937fb2e2ed711b407
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-04-05 13:25:20 -0400

    up

--- BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/BooksAdapter.java
+++ BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/BooksAdapter.java
@@ -12,14 +12,14 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class BooksAdapter extends BaseAdapter implements Filterable {
+public class BooksAdapter extends BaseAdapter  {
 
     Context context;
     ArrayList<Book> books;
 
 
     ArrayList<Book> book_list;
-    customize_filter filter;
+
 
 
     public BooksAdapter(Context context, ArrayList books) {
@@ -65,46 +65,8 @@ public class BooksAdapter extends BaseAdapter implements Filterable {
         return convertView;
     }
 
-    @Override
-    public Filter getFilter() {
-        if (filter == null) {
-            filter = new customize_filter();
-        }
-        return filter;
-    }
-
-    //inner class
-    class customize_filter extends Filter {
-
-        @Override
-        protected FilterResults performFiltering(CharSequence constraint) {
-            FilterResults results = new FilterResults();
-            if (constraint != null && constraint.length() > 0) {
-                constraint = constraint.toString().toUpperCase();
-                ArrayList<Book> found_collection = new ArrayList<Book>();
-                for (int i = 0; i < book_list.size(); i++) {
-                    if (book_list.get(i).getAUTHOR().toUpperCase().contains(constraint)) {
-                        found_collection.add(book_list.get(i));
-
-                    }
-                }
-                results.count = found_collection.size();
-                results.values = found_collection;
-            } else {
-                results.count = book_list.size();
-                results.values = book_list;
-            }
 
 
-            return results;
-        }
-
-        @Override
-        protected void publishResults(CharSequence constraint, FilterResults results) {
-            books = (ArrayList<Book>) results.values;
-            notifyDataSetChanged();
-        }
-    }
 
 
 }
--- BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/MainActivity.java
+++ BookShelf_Base_Assignment 8/app/src/main/java/com/example/bookshelf/MainActivity.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Filter;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -27,54 +26,49 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
 
     BookDetailsFragment bookDetailsFragment;
     final String url = "https://kamorris.com/lab/abp/booksearch.php?search=";
-    String global_url;
     FragmentManager fm;
     boolean twoPane;
     Context context = this;
     RequestQueue requestQueue;
+
     final String url_passing_key = "url_key";
+    final String collection_passing_key = "collection_key";
+    public String global_url = "https://kamorris.com/lab/abp/booksearch.php?search=";
+    public ArrayList<Book> global_books_collection = new ArrayList<>();
 
 
     @Override
     protected void onSaveInstanceState(final Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString(url_passing_key,global_url);
+        outState.putString(url_passing_key, global_url);
+        outState.putParcelableArrayList(collection_passing_key, global_books_collection);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        twoPane = findViewById(R.id.container2) != null;
 
+        create_list();
+        System.out.println("the size " + global_books_collection.size());
 
-        if(savedInstanceState!=null){
-            global_url = savedInstanceState.getString(url_passing_key);
-            System.out.println("your saving url is " + global_url);
-        }else{
-            System.out.println("you have nothing to save");
-        }
-
-        get_data();
-
-        /*
-        get_data();
 
         twoPane = findViewById(R.id.container2) != null;
         fm = getSupportFragmentManager();
+        fm.beginTransaction().replace(R.id.container1, BookListFragment.newInstance(global_books_collection)).commit();
 
-        //fm.beginTransaction().replace(R.id.container1, BookListFragment.newInstance(books_collection)).commit();
+
         if (twoPane) {
             bookDetailsFragment = new BookDetailsFragment();
             fm.beginTransaction().replace(R.id.container2, bookDetailsFragment).commit();
         }
-        */
+
 
     }
 
 
-    public void create_list() {
-        requestQueue = Volley.newRequestQueue(context);
+    public boolean create_list() {
+        requestQueue = Volley.newRequestQueue(this);
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(global_url, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
@@ -95,32 +89,41 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
                     }
                 }
 
+                PASS_COLLECTION pass_collection = new PASS_COLLECTION(global_books_collection);
+                pass_collection.execute();
+
+
             }
+
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 System.out.println("connection fail");
             }
         });
+
         requestQueue.add(jsonArrayRequest);
+        return true;
     }
 
 
-    private void get_data() {
+    private void set_onclick() {
 
         Button button = findViewById(R.id.search_button);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                System.out.println("you click it");
                 EditText mytext = findViewById(R.id.my_search_bar);
                 String user_input = mytext.getText().toString();
-                if(user_input.length() > 0){
+                if (user_input.length() > 0) {
                     global_url = url + user_input;
-                    PASS_DATA pass_data = new PASS_DATA(global_url);
+                    PASS_URL_DATA pass_data = new PASS_URL_DATA(global_url);
                     pass_data.execute();
                     create_list();
-                }else{
-                    PASS_DATA pass_data = new PASS_DATA(url);
+                } else {
+                    PASS_URL_DATA pass_data = new PASS_URL_DATA(url);
                     pass_data.execute();
                     create_list();
                 }
@@ -144,17 +147,33 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
         }
     }
 
-    public class PASS_DATA extends AsyncTask<String, String, String> {
+    public class PASS_URL_DATA extends AsyncTask<String, String, String> {
         String current_data;
 
-        public PASS_DATA(String data) {
-            current_data = data;
+        public PASS_URL_DATA(String url_data) {
+            current_data = url_data;
+
         }
 
         @Override
         protected String doInBackground(String... strings) {
             global_url = current_data;
             return null;
+        }
+    }
+
+    public class PASS_COLLECTION extends AsyncTask<ArrayList<Book>, ArrayList<Book>, ArrayList<Book>> {
+        ArrayList<Book> current_collection;
+
+        public PASS_COLLECTION(ArrayList<Book> collectin_data) {
+            current_collection = collectin_data;
+        }
+
+        @Override
+        protected ArrayList<Book> doInBackground(ArrayList<Book>... arrayLists) {
+            global_books_collection = (ArrayList<Book>) current_collection.clone();
+            return null;
+
         }
     }
 }

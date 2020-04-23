8cd56cedff3759c80ac49e9340cfd32c0b128199
tuh12085@temple.edu <55104326+tuh12085@users.noreply.github.com>
2020-04-23 13:40:37 -0400

    Update MainActivity.java

--- BookShelf_Final_Project/app/src/main/java/com/example/bookshelf/MainActivity.java
+++ BookShelf_Final_Project/app/src/main/java/com/example/bookshelf/MainActivity.java
@@ -321,12 +321,9 @@ public class MainActivity extends AppCompatActivity implements BookListFragment.
 
     }
 
-
     public Book getbook_byID(int book_id){
         for(Book book : books){
             if(book.getId() == book_id){
-
-
                 return book;
             }
         }
